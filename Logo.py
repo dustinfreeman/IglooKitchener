3f7b9f115a0d707fd9c281cfb91ce950d33a5970
Dustin Freeman <dustin.freeman@gmail.com>
2014-05-20 17:45:58 -0400

    Create Logo.py

---
+++ Logo.py
@@ -0,0 +1,14 @@
+import viz
+import vizshape
+import vizact
+
+
+
+Logo = viz.addChild('Meshes/InSofAAvenir.OSGB') 
+Logo.setEuler( 0,-90,0 )
+Logo.color( 0, 0.96, 1 )
+Logo.setScale(100,100,100)
+
+
+
+
