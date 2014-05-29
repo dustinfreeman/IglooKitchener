0094a2cd52c33cf870b7974ce48165a7f10f10cd
Dustin Freeman <dustin.freeman@gmail.com>
2014-05-29 16:03:56 -0400

    communitech cave in separate file

--- Abandoned2/TileTestCAVEversionB.py
+++ Abandoned2/TileTestCAVEversionB.py
@@ -1,236 +1,19 @@
-﻿import viz
-import vizcave
-import viztracker
-import vizact
-import time
+﻿import time
 import array
 import socket
-import artTracker
 import random
 import math
+
+import cave_man
+
 import vizjoy
+joy = vizjoy.add()
+#above will return the first detected joystick
 
 #####################
 import Zeppelin
 from Logo import *
 from Wind import *
-
-viz.go(viz.FULLSCREEN |viz.QUAD_BUFFER)
-viz.window.setFullscreen(1)
-
-#IMPORTANT: Need to add a joystick. This will return the first detected joystick
-joy = vizjoy.add()
-
-#####################
-#setup CAVE
-polyMode = viz.POLY_WIRE
-#viz.setFarPlane(1)
-#viz.setMultiSample(4) #didn't observe a difference in landscapes with anti-aliasing.
-viz.window.setPolyMode(viz.POLY_WIRE)
-viz.vsync(viz.ON)#this may increase frame rate but may get tears
-viz.ipd(0.2) #20 cm apart eyes seemed right.
-
-#####################
-#Setup OSC
-from OSC import OSCClient, OSCMessage 
-#OSCHOST  = "172.16.101.174"
-
-OSCHOST  = "localhost" 
-client = OSCClient()
-client.connect( (OSCHOST, 57120) )
-#####################
-def setCave():
-	global cave, height, width, depth
-	
-	height = 3.049
-	width = 5.638
-	depth = 3.049
-
-	#W,H,D
-	blendVertical=0.19
-	blendHorizontal=0.222
-	A=-width/2,height,0
-	B=-width/2,height,depth
-	C=width/2,height,depth
-	D=width/2,height,0
-
-	E=-width/2,0,0
-	F=-width/2,0,depth
-	G=width/2,0,depth
-	H=width/2,0,0
-
-	AET=-width/2,height/2+blendVertical,0
-	BFT=-width/2,height/2+blendVertical,depth
-	AEB=-width/2,height/2-blendVertical,0
-	BFB=-width/2,height/2-blendVertical,depth
-
-	CGT=width/2,height/2+blendVertical,depth
-	DHT=width/2,height/2+blendVertical,0
-	CGB=width/2,height/2-blendVertical,depth
-	DHB=width/2,height/2-blendVertical,0
-
-	EFT=-width/2,0,depth/2+blendVertical
-	EFB=-width/2,0,depth/2-blendVertical
-
-	GHT=width/2,0,depth/2+blendVertical
-	GHB=width/2,0,depth/2-blendVertical
-
-	BCL=-blendHorizontal,height,depth
-	BCR=+blendHorizontal,height,depth
-	FGL=-blendHorizontal,0,depth
-	FGR=+blendHorizontal,0,depth
-
-	FET=-width/2,0,depth/2+blendVertical
-	FEB=-width/2,0,depth/2-blendVertical
-
-	EHL=-blendHorizontal,0,0
-	EHR=blendHorizontal,0,0
-
-	GHT=width/2,0,depth/2+blendVertical
-	GHB=width/2,0,depth/2-blendVertical
-
-	BCFGTL=-blendHorizontal,height/2+blendVertical,depth
-	BCFGTR=+blendHorizontal,height/2+blendVertical,depth
-	BCFGBL=-blendHorizontal,height/2-blendVertical,depth
-	BCFGBR=+blendHorizontal,height/2-blendVertical,depth
-
-	FGEHTL=-blendHorizontal,0,depth/2+blendVertical
-	FGEHTR=+blendHorizontal,0,depth/2+blendVertical
-	FGEHBL=-blendHorizontal,0,depth/2-blendVertical
-	FGEHBR=+blendHorizontal,0,depth/2-blendVertical
-
-	#-w,h,d
-	Ch1 = vizcave.Wall(
-		upperLeft=A,
-		upperRight=B,
-		lowerLeft=AEB,
-		lowerRight=BFB,
-		name='Left Top Wall' )
-
-	Ch2 = vizcave.Wall(
-		upperLeft=AET,
-		upperRight=BFT,
-		lowerLeft=E,
-		lowerRight=F,
-		name='Left Bottom Wall' )
-
-	Ch3 = vizcave.Wall(
-		upperLeft=B,
-		upperRight=BCR,
-		lowerLeft=BFB,
-		lowerRight=BCFGBR,
-		name='Front TL Wall' )
-
-	Ch4 = vizcave.Wall(
-		upperLeft=BFT,
-		upperRight=BCFGTR,
-		lowerLeft=F,
-		lowerRight=FGR,
-		name='Front BL Wall' )
-
-	Ch5 = vizcave.Wall(
-		upperLeft=BCL,
-		upperRight=C,
-		lowerLeft=BCFGBL,
-		lowerRight=CGB,
-		name='Front TR Wall' )
-
-	Ch6 = vizcave.Wall(
-		upperLeft=BCFGTL,
-		upperRight=CGT,
-		lowerLeft=FGL,
-		lowerRight=G,
-		name='Front BR Wall' )
-
-	Ch7 = vizcave.Wall(
-		upperLeft=C,
-		upperRight=D,
-		lowerLeft=CGB,
-		lowerRight=DHB,
-		name='Right Top Wall' )
-
-	Ch8 = vizcave.Wall(
-		upperLeft=CGT,
-		upperRight=DHT,
-		lowerLeft=G,
-		lowerRight=H,
-		name='Right Bottom Wall' )
-
-	Ch9 = vizcave.Wall(
-		upperLeft=F,
-		upperRight=FGR,
-		lowerLeft=FEB,
-		lowerRight=FGEHBR,
-		name='Bottom TL Wall' )
-
-	Ch10 = vizcave.Wall(
-		upperLeft=FET,
-		upperRight=FGEHTR,
-		lowerLeft=E,
-		lowerRight=EHR,
-		name='Bottom BL Wall' )
-
-	Ch11 = vizcave.Wall(
-		upperLeft=FGL,
-		upperRight=G,
-		lowerLeft=FGEHBL,
-		lowerRight=GHB,
-		name='Bottom TR Wall' )
-
-	Ch12 = vizcave.Wall(
-		upperLeft=FGEHTL,
-		upperRight=GHT,
-		lowerLeft=EHL,
-		lowerRight=H,
-		name='Bottom BR Wall' )
-
-	FrontWall = vizcave.Wall(
-		upperLeft=B,
-		upperRight=C,
-		lowerLeft=F,
-		lowerRight=G,
-		name='Front Wall' )
-
-	cave.addWall(FrontWall, mask=viz.MASTER)
-	cave.addWall(Ch1, mask=viz.CLIENT1)
-	cave.addWall(Ch2, mask=viz.CLIENT2)
-	cave.addWall(Ch3, mask=viz.CLIENT3)
-	cave.addWall(Ch4, mask=viz.CLIENT4)
-	cave.addWall(Ch5, mask=viz.CLIENT5)
-	cave.addWall(Ch6, mask=viz.CLIENT6)
-	cave.addWall(Ch7, mask=viz.CLIENT7)
-	cave.addWall(Ch8, mask=viz.CLIENT8)
-	cave.addWall(Ch9, mask=viz.CLIENT9)
-	cave.addWall(Ch10, mask=1024)
-	cave.addWall(Ch11, mask=2048)
-	cave.addWall(Ch12, mask=4096)
-
-	global viewTracker
-	global cave_origin, counter
-	magicCarpet = viztracker.KeyboardMouse6DOF() #not sure what this is
-	viewTracker = viztracker.KeyboardMouse6DOF() #the control that comes with the cave.
-	viewTracker.setPosition(0,0,0)
-	cave.setTracker(pos=viewTracker)
-	
-	cave_origin = vizcave.CaveView(viewTracker)
-	view = viz.MainView
-	counter = 0
-
-#artracker constants
-speed = 2000000 # move speed
-rot_speed = 17 # rotate speed
-def artTrackerUpdate():
-	
-#	print viz.elapsed() #about 0.003 @ 30 fps
-	elapsed = viz.elapsed()
-	fps_speed = elapsed*speed
-	fps_rot_speed = elapsed*rot_speed
-	
-	viewTracker.setPosition(artTracker.x,artTracker.y,artTracker.z+depth/2)
-	viewTracker.setEuler(artTracker.yaw,artTracker.pitch,artTracker.roll)
-	
-	cave_origin.setPosition(artTracker.jy*artTracker.x2*fps_speed, artTracker.jy*artTracker.y2*fps_speed, artTracker.jy*artTracker.z2*fps_speed,viz.REL_LOCAL)
-	cave_origin.setEuler(fps_rot_speed*artTracker.jx,0.0,0.0,viz.REL_LOCAL)	 
 	
 ####################
 
@@ -413,11 +196,20 @@ vizact.ontimer(0.1,LandVisible)
 ###########################################
 # prints out the position and rotation of the zep
 # also, sends periodic position data to OSC.
+
+#Setup OSC
+from OSC import OSCClient, OSCMessage 
+#OSCHOST  = "172.16.101.174"
+
+OSCHOST  = "localhost" 
+client = OSCClient()
+client.connect( (OSCHOST, 57120) )
+
 def printPOSITION ():
 	ZEPPOSITION = Zeppelin.MAGIC.getPosition()
 	ZEPROTATION = (Zeppelin.MAGIC.getEuler()[0])/360.0 + 180.0 #normalized 0..1
-	CAVEPOS = cave_origin.getPosition()
-	CAVEROT = cave_origin.getEuler()
+	CAVEPOS = cave.cave_origin.getPosition()
+	CAVEROT = cave.cave_origin.getEuler()
 
 	view = view = viz.MainView	
 
@@ -598,13 +390,13 @@ def start_autopilot():
 		
 #set blimp to start in the middle of the map
 def to_start_location():
-	cave_origin.setPosition(AUTOPILOT_TO_POS())
-	cave_origin.setEuler(35,0,0)
+	cave.cave_origin.setPosition(AUTOPILOT_TO_POS())
+	cave.cave_origin.setEuler(35,0,0)
 	
 	start_autopilot()
 
 def steeringWheel():
-	#move the cave_origin around based on the steering wheel
+	#move the cave.cave_origin around based on the steering wheel
 	global SPEED_FACTOR
 	global blimp_speed
 	global climb_speed
@@ -618,8 +410,8 @@ def steeringWheel():
 	
 	elapsed = viz.elapsed()
 	
-	cave_pos = cave_origin.getPosition()
-	cave_eul = cave_origin.getEuler()
+	cave_pos = cave.cave_origin.getPosition()
+	cave_eul = cave.cave_origin.getEuler()
 
 	# ---------------------------------
 	#default control values
@@ -768,13 +560,13 @@ def steeringWheel():
 	blimp_speed = min(MAX_SPEED, blimp_speed)
 	
 	#print "throttle " + str(throttle) + " thrust_accel " + str(thrust_accel) + " blimp_speed " + str(blimp_speed)
-	cave_origin.setPosition(0, 0, blimp_speed*elapsed, viz.REL_LOCAL) 
+	cave.cave_origin.setPosition(0, 0, blimp_speed*elapsed, viz.REL_LOCAL) 
 	
 	#wind
 	#adjust max wind speed based on height
-	wind.MAX_WIND_SPEED = max(IDLE_SPEED*1.5*(cave_origin.getPosition()[1])/unit, 0) + IDLE_SPEED*0.3
+	wind.MAX_WIND_SPEED = max(IDLE_SPEED*1.5*(cave.cave_origin.getPosition()[1])/unit, 0) + IDLE_SPEED*0.3
 	wind.update(elapsed)
-	cave_origin.setPosition(wind.vx, wind.vy, wind.vz, viz.REL_GLOBAL)
+	cave.cave_origin.setPosition(wind.vx, wind.vy, wind.vz, viz.REL_GLOBAL)
 	
 	# ---------------------------------
 	#climb & elevation
@@ -786,7 +578,7 @@ def steeringWheel():
 	
 	climb_speed += CLIMB_ACCEL_RATE*elapsed*climb_actuation
 	climb_speed *= (1 - CLIMB_DAMPING*elapsed)
-	cave_origin.setPosition(0, climb_speed, 0, viz.REL_LOCAL) 
+	cave.cave_origin.setPosition(0, climb_speed, 0, viz.REL_LOCAL) 
 
 	
 	# ---------------------------------
@@ -806,8 +598,8 @@ def steeringWheel():
 		if turn_queue != 0:
 			#print "turn_queue: " + str(turn_queue)
 			pass
-	cave_origin.setEuler(turn_amount, 0, 0, viz.REL_LOCAL)
-	cave_eul = cave_origin.getEuler()
+	cave.cave_origin.setEuler(turn_amount, 0, 0, viz.REL_LOCAL)
+	cave_eul = cave.cave_origin.getEuler()
 	
 	#climbing pitch
 	cave_eul[1] = -climb_speed*CLIMB_PITCH_FACTOR
@@ -815,7 +607,7 @@ def steeringWheel():
 	#banking roll
 	cave_eul[2] = -turn_speed*math.sqrt(blimp_speed/MAX_SPEED)*ROLL_FACTOR
 	
-	cave_origin.setEuler(cave_eul)
+	cave.cave_origin.setEuler(cave_eul)
 	
 	#indicators & other UI
 	
@@ -829,7 +621,7 @@ def steeringWheel():
 	
 	# ---------------------------------
 	#safety bounds check for the entire map.
-	cave_pos = cave_origin.getPosition()
+	cave_pos = cave.cave_origin.getPosition()
 	if cave_pos[0] < -unit*0.5 or cave_pos[0] > unit*(columns - 1 + 0.5) or\
 		cave_pos[1] < -unit*0.5 or cave_pos[1] > unit*1.0 or\
 		cave_pos[2] < -unit*0.5 or cave_pos[2] > unit*(rows - 1  + 0.5):
@@ -941,37 +733,36 @@ viz.callback(viz.KEYDOWN_EVENT,onKeyDown)
 
 ######################
 #unit = tile size
-viz.fog(0.7*unit, 1.0*unit) #settings seem good
-cave = vizcave.Cave()
-cave.setFarPlane(0.95*unit)##needs the farplane to have compass
-#cave.setNearPlane(0.1)
-setCave()
+cave = cave_man.Cave()
+cave.cave.setFarPlane(0.95*unit) #needs the farplane to see close objects?
+viz.fog(0.7*unit, 1.0*unit) #settings seem goodcave
+cave.setCave()
 
 #setting up objects.
-Logo.setParent(cave_origin)
+Logo.setParent(cave.cave_origin)
 Logo.setPosition(0,0,LOGO_DISTANCE)
 vizact.ontimer(0.0,FadeLogoCheck)
 
 compass = viz.addChild('Meshes/arrow.osgb')
 compass.color(0.2,0.8,0.8)
-compass.setParent(cave_origin)
+compass.setParent(cave.cave_origin)
 compass.setPosition(0, -5, 10)
 compass.setScale(6, 6, 6)
 
 turn_queue_compass = compass.copy()
 turn_queue_compass.color(0.8, 0.2, 0.2)
-turn_queue_compass.setParent(cave_origin)
+turn_queue_compass.setParent(cave.cave_origin)
 turn_queue_compass.setPosition(0, -5.5, 10)
 turn_queue_compass.setScale(6, 6, 6)
 
 windspeed_indicator = vizshape.addArrow(10000)
-windspeed_indicator.setParent(cave_origin)
+windspeed_indicator.setParent(cave.cave_origin)
 windspeed_indicator.setPosition([0.0, -2800.0, 17000.0])
 windspeed_indicator.color(254, 97, 52)
 
 ######################
 #movement with art tracker.
-vizact.ontimer(0.0,artTrackerUpdate)
+vizact.ontimer(0.0,cave.viewTrackerUpdate)
 
 #our steering controls, and assorted UI stuff
 vizact.ontimer(0.0,steeringWheel)
---
+++ Abandoned2/cave_man.py
@@ -0,0 +1,221 @@
+﻿#all the core code associated with the cave in Communitech
+import viz
+import vizcave
+import viztracker
+import vizact
+import artTracker
+
+class Cave:
+	
+	def __init__(self):
+
+		viz.go(viz.FULLSCREEN |viz.QUAD_BUFFER)
+		viz.window.setFullscreen(1)
+
+		#####################
+		#setup CAVE
+		polyMode = viz.POLY_WIRE
+		#viz.setFarPlane(1)
+		#viz.setMultiSample(4) #didn't observe a difference in landscapes with anti-aliasing.
+		viz.window.setPolyMode(viz.POLY_WIRE)
+		viz.vsync(viz.ON)#this may increase frame rate but may get tears
+		viz.ipd(0.2) #20 cm apart eyes seemed right for our scaling.
+
+		self.cave = vizcave.Cave()
+		#cave.setNearPlane(0.1)
+
+	#####################
+	def setCave(self):
+		global height, width, depth
+		
+		height = 3.049
+		width = 5.638
+		depth = 3.049
+
+		#W,H,D
+		blendVertical=0.19
+		blendHorizontal=0.222
+		A=-width/2,height,0
+		B=-width/2,height,depth
+		C=width/2,height,depth
+		D=width/2,height,0
+
+		E=-width/2,0,0
+		F=-width/2,0,depth
+		G=width/2,0,depth
+		H=width/2,0,0
+
+		AET=-width/2,height/2+blendVertical,0
+		BFT=-width/2,height/2+blendVertical,depth
+		AEB=-width/2,height/2-blendVertical,0
+		BFB=-width/2,height/2-blendVertical,depth
+
+		CGT=width/2,height/2+blendVertical,depth
+		DHT=width/2,height/2+blendVertical,0
+		CGB=width/2,height/2-blendVertical,depth
+		DHB=width/2,height/2-blendVertical,0
+
+		EFT=-width/2,0,depth/2+blendVertical
+		EFB=-width/2,0,depth/2-blendVertical
+
+		GHT=width/2,0,depth/2+blendVertical
+		GHB=width/2,0,depth/2-blendVertical
+
+		BCL=-blendHorizontal,height,depth
+		BCR=+blendHorizontal,height,depth
+		FGL=-blendHorizontal,0,depth
+		FGR=+blendHorizontal,0,depth
+
+		FET=-width/2,0,depth/2+blendVertical
+		FEB=-width/2,0,depth/2-blendVertical
+
+		EHL=-blendHorizontal,0,0
+		EHR=blendHorizontal,0,0
+
+		GHT=width/2,0,depth/2+blendVertical
+		GHB=width/2,0,depth/2-blendVertical
+
+		BCFGTL=-blendHorizontal,height/2+blendVertical,depth
+		BCFGTR=+blendHorizontal,height/2+blendVertical,depth
+		BCFGBL=-blendHorizontal,height/2-blendVertical,depth
+		BCFGBR=+blendHorizontal,height/2-blendVertical,depth
+
+		FGEHTL=-blendHorizontal,0,depth/2+blendVertical
+		FGEHTR=+blendHorizontal,0,depth/2+blendVertical
+		FGEHBL=-blendHorizontal,0,depth/2-blendVertical
+		FGEHBR=+blendHorizontal,0,depth/2-blendVertical
+
+		#-w,h,d
+		Ch1 = vizcave.Wall(
+			upperLeft=A,
+			upperRight=B,
+			lowerLeft=AEB,
+			lowerRight=BFB,
+			name='Left Top Wall' )
+
+		Ch2 = vizcave.Wall(
+			upperLeft=AET,
+			upperRight=BFT,
+			lowerLeft=E,
+			lowerRight=F,
+			name='Left Bottom Wall' )
+
+		Ch3 = vizcave.Wall(
+			upperLeft=B,
+			upperRight=BCR,
+			lowerLeft=BFB,
+			lowerRight=BCFGBR,
+			name='Front TL Wall' )
+
+		Ch4 = vizcave.Wall(
+			upperLeft=BFT,
+			upperRight=BCFGTR,
+			lowerLeft=F,
+			lowerRight=FGR,
+			name='Front BL Wall' )
+
+		Ch5 = vizcave.Wall(
+			upperLeft=BCL,
+			upperRight=C,
+			lowerLeft=BCFGBL,
+			lowerRight=CGB,
+			name='Front TR Wall' )
+
+		Ch6 = vizcave.Wall(
+			upperLeft=BCFGTL,
+			upperRight=CGT,
+			lowerLeft=FGL,
+			lowerRight=G,
+			name='Front BR Wall' )
+
+		Ch7 = vizcave.Wall(
+			upperLeft=C,
+			upperRight=D,
+			lowerLeft=CGB,
+			lowerRight=DHB,
+			name='Right Top Wall' )
+
+		Ch8 = vizcave.Wall(
+			upperLeft=CGT,
+			upperRight=DHT,
+			lowerLeft=G,
+			lowerRight=H,
+			name='Right Bottom Wall' )
+
+		Ch9 = vizcave.Wall(
+			upperLeft=F,
+			upperRight=FGR,
+			lowerLeft=FEB,
+			lowerRight=FGEHBR,
+			name='Bottom TL Wall' )
+
+		Ch10 = vizcave.Wall(
+			upperLeft=FET,
+			upperRight=FGEHTR,
+			lowerLeft=E,
+			lowerRight=EHR,
+			name='Bottom BL Wall' )
+
+		Ch11 = vizcave.Wall(
+			upperLeft=FGL,
+			upperRight=G,
+			lowerLeft=FGEHBL,
+			lowerRight=GHB,
+			name='Bottom TR Wall' )
+
+		Ch12 = vizcave.Wall(
+			upperLeft=FGEHTL,
+			upperRight=GHT,
+			lowerLeft=EHL,
+			lowerRight=H,
+			name='Bottom BR Wall' )
+
+		FrontWall = vizcave.Wall(
+			upperLeft=B,
+			upperRight=C,
+			lowerLeft=F,
+			lowerRight=G,
+			name='Front Wall' )
+
+		self.cave.addWall(FrontWall, mask=viz.MASTER)
+		self.cave.addWall(Ch1, mask=viz.CLIENT1)
+		self.cave.addWall(Ch2, mask=viz.CLIENT2)
+		self.cave.addWall(Ch3, mask=viz.CLIENT3)
+		self.cave.addWall(Ch4, mask=viz.CLIENT4)
+		self.cave.addWall(Ch5, mask=viz.CLIENT5)
+		self.cave.addWall(Ch6, mask=viz.CLIENT6)
+		self.cave.addWall(Ch7, mask=viz.CLIENT7)
+		self.cave.addWall(Ch8, mask=viz.CLIENT8)
+		self.cave.addWall(Ch9, mask=viz.CLIENT9)
+		self.cave.addWall(Ch10, mask=1024)
+		self.cave.addWall(Ch11, mask=2048)
+		self.cave.addWall(Ch12, mask=4096)
+
+		global viewTracker
+		#global cave_origin, counter
+		magicCarpet = viztracker.KeyboardMouse6DOF() #not sure what this is
+		viewTracker = viztracker.KeyboardMouse6DOF() #the control that comes with the cave.
+		viewTracker.setPosition(0,0,0)
+		self.cave.setTracker(pos=viewTracker)
+		
+		self.cave_origin = vizcave.CaveView(viewTracker)
+		view = viz.MainView
+		counter = 0
+
+	def viewTrackerUpdate(self):
+		#viewTracker constants
+		speed = 2000000 # move speed
+		rot_speed = 17 # rotate speed
+		
+		
+	#	print viz.elapsed() #about 0.003 @ 30 fps
+		elapsed = viz.elapsed()
+		fps_speed = elapsed*speed
+		fps_rot_speed = elapsed*rot_speed
+		
+		viewTracker.setPosition(artTracker.x,artTracker.y,artTracker.z+depth/2)
+		viewTracker.setEuler(artTracker.yaw,artTracker.pitch,artTracker.roll)
+		
+		self.cave_origin.setPosition(artTracker.jy*artTracker.x2*fps_speed, artTracker.jy*artTracker.y2*fps_speed, artTracker.jy*artTracker.z2*fps_speed,viz.REL_LOCAL)
+		self.cave_origin.setEuler(fps_rot_speed*artTracker.jx,0.0,0.0,viz.REL_LOCAL)	 
+
